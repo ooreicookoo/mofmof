d02312422ff1984a5f050a917e8bd68038e542a1
reiko enomoto <ooreicookoo@gmail.com>
2020-11-05 16:04:31 +0900

    ADD:nest to routing

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,67 +1,43 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
     @nearest_stations = @property.nearest_stations
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
-    2.times { @property.nearest_stations.build}
+    2.times { @property.nearest_stations.build }
   end
 
-  # GET /properties/1/edit
   def edit
     @property.nearest_stations.build
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
-
-    respond_to do |format|
-      if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
-        format.json { render :show, status: :created, location: @property }
-      else
-        format.html { render :new }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.save
+      redirect_to properties_path, notice: "物件を登録しました"
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
-    respond_to do |format|
       if @property.update(property_params)
-        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
-        format.json { render :show, status: :ok, location: @property }
-      else
-        format.html { render :edit }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+        redirect_to properties_path, notice: "物件を編集しました！"
+    else
+      render :edit
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
-    respond_to do |format|
-      format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to properties_path, notice: "物件を削除しました！"
   end
 
   private
